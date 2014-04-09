ba6713d2af33e43bb2c7b53a5619e6764a386b22
Neil Mithipati <neil.mithipati@gmail.com>
2014-04-09 01:12:30 -0500

    Start Angular Exercise

--- app.js
+++ app.js
@@ -1,13 +1,1 @@
-var todoApp = angular.module("todoApp", []);
 
-todoApp.controller("TodoController", function($scope) {
-
-  $scope.query = "";
-
-  $scope.items = [
-    { detail: "Eat", done: false },
-    { detail: "Sleep", done: false }
-  ];
-
-
-});
