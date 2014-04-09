56630939c49401973240eacab5823fcf2caac209
Neil Mithipati <neil.mithipati@gmail.com>
2014-04-09 13:30:51 -0500

    Resources are key

--- README.md
+++ README.md
@@ -5,6 +5,4 @@
 
 Welcome to this brief introduction to Angular.
 
-Below are useful resources I found along the way when exploring this new technology.
 
-+ []
--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+var resources = angular.module("resources", []);
 
+resources.controller("ResourceController", function ($scope) {
+  $scope.linkList = [
+    { name: "ngTalk Extension" , url: "https://github.com/mithipati/ngTalkExt" },
+    { name: "Thinkster Tutorial" , url: "http://www.thinkster.io/angularjs/GtaQ0oMGIl/a-better-way-to-learn-angularjs" },
+    { name: "egghead screen casts" , url: "https://egghead.io/" },
+    { name: "ng Newsletter" , url: "http://www.ng-newsletter.com/" },
+    { name: "ng Magazine" , url: "https://flipboard.com/section/the-angularjs-magazine-bbIMWS" },
+    { name: "Angular.js Repo" , url: "https://github.com/jmcunningham/AngularJS-Learning" },
+    { name: "AngularJS: The Awesome Parts by Ari Lerney" , url: "http://vimeo.com/90388983" }
+  ];
+});
