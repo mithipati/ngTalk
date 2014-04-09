9a52ce721350024ab25d8920c61f4599e12c81cc
Neil Mithipati <neil.mithipati@gmail.com>
2014-04-09 00:21:25 -0500

    Checking errands off and making progress

--- app.js
+++ app.js
@@ -4,28 +4,15 @@ todoApp.controller("TodoController", function($scope) {
 
   $scope.items = [
     { detail: "Eat", done: false },
-    { detail: "Sleep", done: false }
+    { detail: "Sleep", done: false },
+    { detail: "Code", done: false },
+    { detail: "Repeat", done: false }
   ];
 
-  $scope.newItem = "";
-
   $scope.addItem = function () {
-    // Implement method
-    // Remember, when changing models that will appear in the view, use $scope
+    $scope.items.push({ detail: $scope.newItem, done: false });
+    $scope.newItem = "";
   };
 
-
-
-  // BONUS (read below)
-
-  // Simple boolean expression
-  $scope.present = true;
-
-  // Notice that I still had to append $scope to 'present' when re-assiging its value.
-  // This is because it will effect elements on the DOM.
-  // The $scope is our connection to the view from here.
-  $scope.removeAll = function () {
-    $scope.present = false;
-  };
 
 });
