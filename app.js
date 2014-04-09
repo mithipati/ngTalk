1d39fce0677d548b4310338584b67b5445602b65
Neil Mithipati <neil.mithipati@gmail.com>
2014-04-08 23:58:09 -0500

    Now coding and repeating

--- app.js
+++ app.js
@@ -2,12 +2,30 @@ var todoApp = angular.module("todoApp", []);
 
 todoApp.controller("TodoController", function($scope) {
 
-  $scope.query = "";
-
   $scope.items = [
     { detail: "Eat", done: false },
     { detail: "Sleep", done: false }
   ];
 
+  $scope.newItem = "";
+
+  $scope.addItem = function () {
+    // Implement method
+    // Remember, when changing models that will appear in the view, use $scope
+  };
+
+
+
+  // BONUS (read below)
+
+  // Simple boolean expression
+  $scope.present = true;
+
+  // Notice that I still had to append $scope to 'present' when re-assiging its value.
+  // This is because it will effect elements on the DOM.
+  // The $scope is our connection to the view from here.
+  $scope.removeAll = function () {
+    $scope.present = false;
+  };
 
 });
